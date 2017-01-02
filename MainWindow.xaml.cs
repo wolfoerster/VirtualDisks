0033b14c3c82302df91d8790b9a1c60fa48c3f73
wolfoerster <wolfoerster@gmx.de>
2017-01-02 09:50:13 +0100

    Initial code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,180 +13,169 @@ using System.Text;
 
 namespace VirtualDisks
 {
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-			InitializeRecords();
-			DataContext = this;
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeRecords();
+            DataContext = this;
 
-			Top = Properties.Settings.Default.Top;
-			Left = Properties.Settings.Default.Left;
-			if (Top <= 0 || Left <= 0)
-				WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            Top = Properties.Settings.Default.Top;
+            Left = Properties.Settings.Default.Left;
+            if (Top <= 0 || Left <= 0)
+                WindowStartupLocation = WindowStartupLocation.CenterScreen;
 
-			Closing += MeClosing;
-		}
+            Closing += MeClosing;
+        }
 
-		void MeClosing(object sender, System.ComponentModel.CancelEventArgs e)
-		{
-			SaveSettings();
-		}
+        void MeClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SaveSettings();
+        }
 
-		void SaveSettings()
-		{
-			string files = "";
-			foreach (var record in Records)
-			{
-				if (record.Mounted)
-					files += record.FileName + "|";
-			}
-			Properties.Settings.Default.MountedFiles = files;
-			Properties.Settings.Default.Left = Left;
-			Properties.Settings.Default.Top = Top;
-			Properties.Settings.Default.Save();
-		}
+        void SaveSettings()
+        {
+            string files = "";
+            foreach (var record in Records)
+            {
+                if (record.Mounted)
+                    files += record.FileName + "|";
+            }
+            Properties.Settings.Default.MountedFiles = files;
+            Properties.Settings.Default.Left = Left;
+            Properties.Settings.Default.Top = Top;
+            Properties.Settings.Default.Save();
+        }
 
-		protected override void OnKeyDown(KeyEventArgs e)
-		{
-			base.OnKeyDown(e);
-			if (e.Key == Key.Escape)
-				Close();
-		}
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            if (e.Key == Key.Escape)
+                Close();
+        }
 
-		public List<Record> Records { get; set; }
+        public List<Record> Records { get; set; }
 
-		void InitializeRecords()
-		{
-			Records = new List<Record>();
-			Record.OnMountedChanged = MountVHD;
+        void InitializeRecords()
+        {
+            Records = new List<Record>();
+            Record.OnMountedChanged = MountVHD;
 
-			DriveInfo[] infos = DriveInfo.GetDrives();
-			foreach (var info in infos)
-			{
-				if (string.IsNullOrWhiteSpace(info.Name)
-					|| info.Name.StartsWith("A:")
-					|| info.Name.StartsWith("B:")
-					|| !info.IsReady)
-					continue;
+            DriveInfo[] infos = DriveInfo.GetDrives();
+            foreach (var info in infos)
+            {
+                if (string.IsNullOrWhiteSpace(info.Name)
+                    || info.Name.StartsWith("A:")
+                    || info.Name.StartsWith("B:")
+                    || !info.IsReady)
+                    continue;
 
-				string name = info.Name + "WindowsImageBackup";
-				if (!Directory.Exists(name))
-					continue;
+                string name = info.Name + "WindowsImageBackup";
+                if (!Directory.Exists(name))
+                    continue;
 
-				string[] subdirs = Directory.GetDirectories(name);
-				foreach (var subdir in subdirs)
-					CheckDirectory(subdir);
-			}
-		}
+                string[] subdirs = Directory.GetDirectories(name);
+                foreach (var subdir in subdirs)
+                    CheckDirectory(subdir);
+            }
+        }
 
-		void CheckDirectory(string name)
-		{
-			string[] subdirs = Directory.GetDirectories(name);
-			name = subdirs.FirstOrDefault(x => x.IndexOf("\\Backup ") > 0);
-			if (name == null)
-				return;
+        void CheckDirectory(string name)
+        {
+            string[] subdirs = Directory.GetDirectories(name);
+            name = subdirs.FirstOrDefault(x => x.IndexOf("\\Backup ") > 0);
+            if (name == null)
+                return;
 
-			string xml = null;
-			try
-			{
-				xml = File.ReadAllText(name + "\\BackupSpecs.xml", Encoding.Unicode);
-			}
-			catch
-			{
-			}
+            string xml = null;
+            try
+            {
+                xml = File.ReadAllText(name + "\\BackupSpecs.xml", Encoding.Unicode);
+            }
+            catch
+            {
+            }
 
-			string[] files = Directory.GetFiles(name, "*.vhd");
-			List<Record> records = new List<Record>();
+            string[] files = Directory.GetFiles(name, "*.vhd");
+            List<Record> records = new List<Record>();
 
-			foreach (var file in files)
-			{
-				string path = FindPath(xml, file);
-				if (path.Length == 3)
-				{
-					Record record = new Record(file, path, Properties.Settings.Default.MountedFiles.Contains(file));
-					records.Add(record);
-				}
-			}
+            foreach (var file in files)
+            {
+                string path = FindPath(xml, file);
+                if (path.Length == 3)
+                {
+                    Record record = new Record(file, path, Properties.Settings.Default.MountedFiles.Contains(file));
+                    records.Add(record);
+                }
+            }
 
-			records.Sort((x, y) => x.Path.CompareTo(y.Path));
-			for (int i = 1; i < records.Count; i++)
-				records[i].ShortName = records[i].Date = "";
-			Records.AddRange(records);
-		}
+            records.Sort((x, y) => x.Path.CompareTo(y.Path));
+            for (int i = 1; i < records.Count; i++)
+                records[i].ShortName = records[i].Date = "";
+            Records.AddRange(records);
+        }
 
-		private string FindPath(string xml, string fileName)
-		{
-			if (xml == null)
-				return "";
+        private string FindPath(string xml, string fileName)
+        {
+            if (xml == null)
+                return "";
 
-			string name = Path.GetFileNameWithoutExtension(fileName);
-			int i = xml.IndexOf(name);
-			if (i < 0)
-				return "";
-			i += name.Length;
+            string name = Path.GetFileNameWithoutExtension(fileName);
+            int i = xml.IndexOf(name);
+            if (i < 0)
+                return "";
+            i += name.Length;
 
-			string str = "FilePath=\"";
-			i = xml.IndexOf(str, i);
-			if (i < 0)
-				return "";
-			i += str.Length;
+            string str = "FilePath=\"";
+            i = xml.IndexOf(str, i);
+            if (i < 0)
+                return "";
+            i += str.Length;
 
-			int j = xml.IndexOf("\"", i);
-			if (j < 0)
-				return "";
+            int j = xml.IndexOf("\"", i);
+            if (j < 0)
+                return "";
 
-			string path = xml.Substring(i, j - i);
-			return path;
-		}
+            string path = xml.Substring(i, j - i);
+            return path;
+        }
 
-		bool MountVHD(string fileName, bool mode)
-		{
-			if (!File.Exists(fileName))
-				return false;
+        bool MountVHD(string fileName, bool mode)
+        {
+            if (!File.Exists(fileName))
+                return false;
 
-			SaveSettings();
-			string script = Path.GetTempPath() + "diskpart.txt";
-			File.WriteAllText(script, string.Format("select vdisk file=\"{0}\"\r\n{1} vdisk", fileName, mode ? "attach" : "detach"));
+            SaveSettings();
+            string script = Path.GetTempPath() + "diskpart.txt";
+            File.WriteAllText(script, string.Format("select vdisk file=\"{0}\"\r\n{1} vdisk", fileName, mode ? "attach" : "detach"));
 
-			Process process = new Process();
-			process.StartInfo.FileName = "diskpart.exe";
-			process.StartInfo.Arguments = string.Format("/s \"{0}\"", script);
-			process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-			process.Start();
+            Process process = new Process();
+            process.StartInfo.FileName = "diskpart.exe";
+            process.StartInfo.Arguments = string.Format("/s \"{0}\"", script);
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.Start();
 
-			bool result = false;
-			while (true)
-			{
-				process.WaitForExit(7000);
-				if (process.HasExited)
-				{
-					if (process.ExitCode == 0)
-						result = true;
-					else
-						MessageBox.Show(string.Format("Exit code = {0}", (uint)process.ExitCode), "MountVHD Error");
-					break;
-				}
+            bool result = false;
+            while (true)
+            {
+                process.WaitForExit(7000);
+                if (process.HasExited)
+                {
+                    if (process.ExitCode == 0)
+                        result = true;
+                    else
+                        MessageBox.Show(string.Format("Exit code = {0}", (uint)process.ExitCode), "MountVHD Error");
+                    break;
+                }
 
-				var answer = MessageBox.Show("Mounting the VHD takes long! Wait another 7 seconds?", "MountVHD", MessageBoxButton.YesNo, MessageBoxImage.Question);
-				if (answer == MessageBoxResult.No)
-					break;
-			}
+                var answer = MessageBox.Show("Mounting the VHD takes long! Wait another 7 seconds?", "MountVHD", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (answer == MessageBoxResult.No)
+                    break;
+            }
 
-			File.Delete(script);
-			return result;
-		}
-
-		DriveInfo GetDelta(DriveInfo[] infos1, DriveInfo[] infos2)
-		{
-			foreach (var info2 in infos2)
-			{
-				DriveInfo info1 = infos1.FirstOrDefault(x => x.Name == info2.Name);
-				if (info1 == null)
-					return info2;
-			}
-			return null;
-		}
-	}
+            File.Delete(script);
+            return result;
+        }
+    }
 }
--- Record.cs
+++ Record.cs
@@ -9,87 +9,86 @@ using System.ComponentModel;
 
 namespace VirtualDisks
 {
-	public class Record : INotifyPropertyChanged
-	{
-		#region INotifyPropertyChanged Members
+    public class Record : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Members
 
-		public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
 
-		protected void FirePropertyChanged(string propertyName)
-		{
-			if (PropertyChanged != null && propertyName != null)
-				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-		}
+        protected void FirePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null && propertyName != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
 
-		#endregion
+        #endregion
 
-		public Record(string fileName, string path, bool isMounted)
-		{
-			FileName = fileName;
-			Path = path;
-			mounted = isMounted;
+        public Record(string fileName, string path, bool isMounted)
+        {
+            FileName = fileName;
+            Path = path;
+            mounted = isMounted;
 
-			string dir = "WindowsImageBackup\\";
-			int i = fileName.IndexOf(dir);
-			string name = fileName.Substring(i + dir.Length);
+            string dir = "WindowsImageBackup\\";
+            int i = fileName.IndexOf(dir);
+            string name = fileName.Substring(i + dir.Length);
 
-			dir = "\\Backup ";
-			i = name.IndexOf(dir);
-			ShortName = name.Substring(0, i);
+            dir = "\\Backup ";
+            i = name.IndexOf(dir);
+            ShortName = name.Substring(0, i);
 
-			name = name.Substring(i + dir.Length);
-			i = name.IndexOf(' ');
-			Date = name.Substring(0, i);
+            name = name.Substring(i + dir.Length);
+            i = name.IndexOf(' ');
+            Date = name.Substring(0, i);
 
-			FileInfo info = new FileInfo(fileName);
-			double size = info.Length / 1024.0 / 1024.0 / 1024.0;
-			Size = size.ToString("F3") + " GB";
-		}
+            FileInfo info = new FileInfo(fileName);
+            double size = info.Length / 1024.0 / 1024.0 / 1024.0;
+            Size = size.ToString("F3") + " GB";
+        }
 
-		public string FileName { get; set; }
-		public string ShortName { get; set; }
-		public string Path { get; set; }
-		public string Date { get; set; }
-		public string Size { get; set; }
-		public string NewPath { get; set; }
-		public static Func<string, bool, bool> OnMountedChanged;
+        public string FileName { get; set; }
+        public string ShortName { get; set; }
+        public string Path { get; set; }
+        public string Date { get; set; }
+        public string Size { get; set; }
+        public string NewPath { get; set; }
+        public static Func<string, bool, bool> OnMountedChanged;
 
-		public bool Mounted
-		{
-			get { return mounted; }
-			set
-			{
-				if (mounted != value)
-				{
-					mounted = value;
-					if (OnMountedChanged != null)
-					{
-						DriveInfo[] oldInfos = DriveInfo.GetDrives();
-						if (OnMountedChanged(FileName, mounted))
-						{
-							NewPath = GetNewDrive(oldInfos);
-							FirePropertyChanged("NewPath");
-						}
-					}
-					FirePropertyChanged("Mounted");
-				}
-			}
-		}
-		private bool mounted;
+        public bool Mounted
+        {
+            get { return mounted; }
+            set
+            {
+                if (mounted != value)
+                {
+                    mounted = value;
+                    if (OnMountedChanged != null)
+                    {
+                        DriveInfo[] oldInfos = DriveInfo.GetDrives();
+                        if (OnMountedChanged(FileName, mounted))
+                        {
+                            NewPath = GetNewPath(oldInfos);
+                            FirePropertyChanged("NewPath");
+                        }
+                    }
+                }
+            }
+        }
+        private bool mounted;
 
-		string GetNewDrive(DriveInfo[] oldInfos)
-		{
-			DriveInfo[] newInfos = DriveInfo.GetDrives();
-			if (newInfos.Length <= oldInfos.Length)
-				return "";
+        string GetNewPath(DriveInfo[] oldInfos)
+        {
+            DriveInfo[] newInfos = DriveInfo.GetDrives();
+            if (newInfos.Length <= oldInfos.Length)
+                return "";
 
-			foreach (var newInfo in newInfos)
-			{
-				DriveInfo oldInfo = oldInfos.FirstOrDefault(x => x.Name == newInfo.Name);
-				if (oldInfo == null)
-					return newInfo.Name;
-			}
-			return "";
-		}
-	}
+            foreach (var newInfo in newInfos)
+            {
+                DriveInfo oldInfo = oldInfos.FirstOrDefault(x => x.Name == newInfo.Name);
+                if (oldInfo == null)
+                    return newInfo.Name;
+            }
+            return "";
+        }
+    }
 }
